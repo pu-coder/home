d166228cd2a69d5e40cfb526e54883db9b5f1776
Peerzada Usmaan <70443244+pu-coder@users.noreply.github.com>
2020-09-05 21:32:11 +0530

    Add files via upload

---
+++ pu-coder.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Please enter your name:");
+
+if (firstName != null) {
+  document.getElementById("demo").innerHTML =
+  b("Welcome " + , + firstName + "!" + "," + , + "Thankyou for visiting my website.");
+}
