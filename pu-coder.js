8bc756bf6d3212b57b1e31981d994566cb01bafe
Peerzada Usmaan <70443244+pu-coder@users.noreply.github.com>
2020-09-05 21:30:16 +0530

    Delete pu-coder.js

--- pu-coder.js
+++
@@ -1,6 +0,0 @@
-var firstName = prompt("Please enter your name:");
-
-if (firstName != null) {
-  document.getElementById("demo").innerHTML =
-  b("Welcome " + , + firstName + "!" + "," + , + "Thankyou for visiting my website.");
-}
